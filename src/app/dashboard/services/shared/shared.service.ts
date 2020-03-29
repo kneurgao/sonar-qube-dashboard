b6ce8a4a90eebff8d2e5f3ffc1b7eab35c9ee3a2
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2020-03-29 23:00:55 +0530

    Cached metrics key:value

---
+++ src/app/dashboard/services/shared/shared.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { DashboardModule } from '../../dashboard.module';
+
+@Injectable({
+  providedIn: DashboardModule
+})
+export class SharedService {
+
+  METRICS = {};
+  constructor() { }
+
+  saveMetrics(metrics: any[]) {
+    metrics.forEach(metric => {
+      this.METRICS[metric.key] = metric.name;
+    });
+  }
+
+  getMetric(key: string) {
+    return this.METRICS[key];
+  }
+}
