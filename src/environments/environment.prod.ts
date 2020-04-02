e2cf3bc7e2d8750a146f5443b3b063b566bedd2d
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2020-04-02 15:51:44 +0530

    Added proxy conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+
+  URL_PREFIX: 'http://114.143.20.82:9000/api',
+  AUTH_TOKEN: 'MWExYmY5ODlhMjE3M2EyZDI2MTQxYzgxNzg2NDRmZmI4MWUwZTJjZjo='
 };
