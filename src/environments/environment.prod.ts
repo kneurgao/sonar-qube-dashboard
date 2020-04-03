749046fe73923fef26095226016c62a5140a37c7
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2020-04-03 23:55:38 +0530

    Path configuration for prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 
-  URL_PREFIX: 'http://114.143.20.82:9000/api',
+  URL_PREFIX: 'http://114.143.20.82:9090/sonarqube/api',
   AUTH_TOKEN: 'MWExYmY5ODlhMjE3M2EyZDI2MTQxYzgxNzg2NDRmZmI4MWUwZTJjZjo='
 };
